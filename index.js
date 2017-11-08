91e9536d7bb46699d7d6d20301d14c41e484b93f
Greg Connell <greg.connell@ff0000.com>
2017-11-08 10:07:51 -0800

    externalizing this package

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const path = require('path');
 const { exec } = require('child_process');
 const shellEscape = require('shell-escape');
+const ncp = require('ncp');
 
 const debug = require('debug');
 var log = debug('font-fixer');
 
 function apply(absPath) {
 	return new Promise((resolve, reject) => {
-		const args = shellEscape([
-			path.resolve(`${__dirname}/bin/allow_font_embed`),
-			absPath
-		]);
-		log(args);
-		exec(`${args[0]} ${args[1]}`, (err, stdout, stderr) => {
+		const fixedPath = `${path.dirname(absPath)}/${path.basename(absPath)}__drm-stripped${path.extname(absPath)}`;
+		ncp(absPath, fixedPath, (err) => {
 			if (err) {
 				return reject(err);
 			}
-			resolve();
+			const args = shellEscape([
+				path.resolve(`${__dirname}/bin/allow_font_embed`),
+				fixedPath
+			]);
+			log(args);
+			exec(`${args[0]} ${args[1]}`, (err, stdout, stderr) => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+
 		});
 	});
 }
