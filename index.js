76141259476d820bb95740261ae3249f951ca912
Greg Connell <greg.connell@gregc-mbp.ff0000.com>
2018-04-09 17:23:02 -0700

    repaths import for @ff0000-ad-tech/debug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
 const fontFixer = require('./lib/font-fixer');
 
-const debug = require('debug');
+const debug = require('@ff0000-ad-tech/debug');
 var log = debug('font-fixer');
 
 log('CLI args:')
